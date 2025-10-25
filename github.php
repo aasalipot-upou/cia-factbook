0f868c2d6872b93c768c09cde56f00036de63119
Alma Anne A.Salipot <aasalipot@up.edu.ph>
2025-10-25 21:01:39 +0000

    first commit

--- github.php
+++ github.php
@@ -8,7 +8,7 @@ $hash = 'sha1='.hash_hmac('sha1', $payload, $secret);
 
 if (isset($_SERVER[$header]) && $_SERVER[$header] === $hash) {
     $payload_decoded = json_decode($payload, true);
-    if (isset($payload_decoded['ref']) && $payload_decoded['ref'] === 'refs/heads/main') {
-        shell_exec('cd /var/www/html && git reset --hard HEAD && git fetch origin main && git pull origin main 2>&1');
+    if ($payload_decoded['ref'] === 'refs/heads/main') {
+        shell_exec('git reset --hard HEAD && git fetch origin main && git pull origin main');
     }
 }
